40b32790164e18ebb124e7c29a8af173a09dc133
Harry Xie <harry1996@gmail.com>
2018-10-31 22:03:38 +0000

    Added Regularisation and specifying learning rate.

--- reinforcement_learning/models/dense_model.py
+++ reinforcement_learning/models/dense_model.py
@@ -3,7 +3,6 @@ from typing import Sequence
 
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.python.keras import Sequential
 
 from logger_utils.logger_utils import log_process
 from reinforcement_learning.models.base_model import BaseModel
@@ -14,25 +13,33 @@ logger = logging.getLogger(__name__)
 class DenseModel(BaseModel):
 
     def __init__(self, inputs: int, outputs: int,
-                 layer_nodes: Sequence[int] = (64, 64, 64),
-                 inner_activation=tf.nn.relu, output_activation=tf.nn.sigmoid):
+                 layer_nodes: Sequence[int] = (24, 24),
+                 inner_activation=tf.nn.relu, output_activation='linear',
+                 regularizer=keras.regularizers.l2(0.01),
+                 learning_rate=0.003):
         logger.info(f'Creating DenseModel with {inputs} inputs and {outputs} outputs.')
         self.inputs = inputs
         self.outputs = outputs
         self.layer_nodes = layer_nodes
         self.inner_activation = inner_activation
         self.output_activation = output_activation
+        self.regularizer = regularizer
+        self.learning_rate = learning_rate
+
         super().__init__()
 
     @log_process(logger, 'building model')
-    def build_model(self) -> Sequential:
+    def build_model(self) -> keras.Sequential:
         model = keras.Sequential()
 
         model.add(keras.layers.Dense(self.inputs))
 
         for _layer_nodes in self.layer_nodes:
-            model.add(keras.layers.Dense(_layer_nodes, activation=self.inner_activation))
+            model.add(
+                keras.layers.Dense(_layer_nodes, activation=self.inner_activation, kernel_regularizer=self.regularizer)
+            )
 
         model.add(keras.layers.Dense(self.outputs, activation=self.output_activation))
-        model.compile(loss='mse', optimizer='adam', metrics=['mae'])
+        optimizer = keras.optimizers.Adam(lr=self.learning_rate)
+        model.compile(loss='mse', optimizer=optimizer, metrics=['mae'])
         return model
