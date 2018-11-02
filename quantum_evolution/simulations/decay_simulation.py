e0906f0737815580c068309b5ffbbfb669c90780
Harry Xie <harry1996@gmail.com>
2018-11-02 17:44:43 +0000

    decay_simulation minor improvements.

--- quantum_evolution/simulations/decay_simulation.py
+++ quantum_evolution/simulations/decay_simulation.py
@@ -9,6 +9,7 @@ from quantum_evolution.simulations.base_simulation import HamiltonianData, BaseS
 logger = logging.getLogger()
 logging.basicConfig(level=logging.INFO)
 
+# TODO: Improve name, give proper description, implement proper scenario.
 
 def H1_coeff(t, args):
     return -0.1 * t
@@ -16,7 +17,7 @@ def H1_coeff(t, args):
 
 DEFAULT_HAMILTONIAN_DATA = HamiltonianData(sigmax(), H1_coeff)
 
-psi0 = rand_dm(2).unit()
+psi0 = rand_ket(2)
 t_list = np.linspace(0, 2 * np.pi, 200)
 e_ops = [sigmax(), sigmay(), sigmaz()]
 decay_simulation = BaseSimulation([DEFAULT_HAMILTONIAN_DATA], psi0, t_list=t_list, e_ops=e_ops)
