402c24b59065528e436d4925a2829c20cea6a031
Harry Xie <harry1996@gmail.com>
2018-11-03 17:49:52 +0000

    Fixed tests.

--- reinforcement_learning/test_base_classes.py
+++ reinforcement_learning/test_base_classes.py
@@ -1,8 +1,12 @@
 import unittest
 
+from qutip import rand_ket
+
+from quantum_evolution.envs.base_pseudo_env import BasePseudoEnv
 from reinforcement_learning.models.base_model import BaseModel
 from reinforcement_learning.models.dense_model import DenseModel
 from reinforcement_learning.trainers.base_trainer import BaseTrainer
+from reinforcement_learning.trainers.hyperparameters import QLearningHyperparameters
 
 
 class TestClasses(unittest.TestCase):
@@ -10,9 +14,13 @@ class TestClasses(unittest.TestCase):
         with self.assertRaises(NotImplementedError):
             BaseModel()
 
+    def test_base_pseudo_env(self):
+        with self.assertRaises(NotImplementedError):
+            BasePseudoEnv(rand_ket(2), rand_ket(2))
+
     def test_base_trainer(self):
         model = DenseModel(5, 5)
-        trainer = BaseTrainer(model, None)
+        trainer = BaseTrainer(model, None, QLearningHyperparameters(0.8), False)
 
         with self.assertRaises(NotImplementedError):
             trainer.train()
