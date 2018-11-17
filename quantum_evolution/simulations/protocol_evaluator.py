2caa3ab2897d93d6f60624ae8e099116a1cd358a
Harry Xie <harry1996@gmail.com>
2018-11-17 16:01:02 +0000

    Renamed protocol_evaluator, added comments. Also fixed starting sign of h_x by moving minus to the hamiltonian, not the function.

--- quantum_evolution/simulations/protocol_evaluator.py
+++ quantum_evolution/simulations/protocol_evaluator.py
@@ -1,4 +1,4 @@
-from typing import Sequence, Callable
+from typing import Sequence, Callable, Any
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,7 +8,7 @@ from quantum_evolution.plotter.bloch_animator import BlochAnimator
 from quantum_evolution.simulations.base_simulation import HamiltonianData
 
 
-def test_protocol(
+def evaluate_protocol(
         protocol: Sequence[int],
         hamiltonian: Sequence[HamiltonianData],
         t: float,
@@ -33,7 +33,7 @@ def test_protocol(
         options=Options(store_states=True)
     )
 
-    fidelity_ = fidelity(result.states[-1], target_state)
+    fidelity_ = fidelity(result.states[-1], target_state) ** 2
     print(f"fidelity: {fidelity_}")
 
     bloch_animation = BlochAnimator([result], static_states=[initial_state, target_state])
@@ -42,8 +42,13 @@ def test_protocol(
 
 
 def get_h_list(protocol: Sequence[int]) -> Sequence[int]:
+    """
+    Gets a list of h_x's for the given protocol.
+    :param protocol:
+    :return:
+    """
     h_list = []
-    current_h_x = 4
+    current_h_x = -4
 
     for i in range(len(protocol)):
         if protocol[i] == 1:
@@ -53,22 +58,35 @@ def get_h_list(protocol: Sequence[int]) -> Sequence[int]:
     return h_list
 
 
-def get_H1_coeff(t: float, N: int, h_list: Sequence[int]) -> Callable:
+def get_H1_coeff(t: float, N: int, h_list: Sequence[int]) -> Callable[[float, Any], float]:
+    """
+    Gets the H1_coeff function that is required for the hamiltonian.
+    :param t: Total duration
+    :param N: Number of steps
+    :param h_list: List of h_x's (given from get_h_list())
+    :return: H1_coeff - a function that returns the coefficient of the time-dependent part of the hamiltonian.
+    """
     t_list = np.linspace(0, t, N + 1)
 
-    def H1_coeff(t, args):
+    def H1_coeff(t: float, args: Any) -> float:
         if t < 0:
             return 0
         if t > t_list[-1]:
             return 0
 
         index = int(np.argmax(t_list > t) - 1)
-        return -h_list[index]
+        return h_list[index]
 
     return H1_coeff
 
 
 def plot_h(t: float, protocol: Sequence[int]):
+    """
+    Plots h_x as a function of time (as per paper)
+    :param t:
+    :param protocol:
+    :return:
+    """
     fig = plt.figure(figsize=(10, 4))
     ax = plt.gca()
 
@@ -78,7 +96,7 @@ def plot_h(t: float, protocol: Sequence[int]):
 
     H1_coeff = get_H1_coeff(t, len(protocol), h_list)
     t_list = np.linspace(0, t, len(protocol) * 20)
-    plt.plot(t_list, [-H1_coeff(t, None) for t in t_list], '-')
+    plt.plot(t_list, [H1_coeff(t, None) for t in t_list], '-')
 
     ax.set_xlim([0, t * 1.1])
     ax.set_ylim([-4.2, 4.2])
@@ -97,31 +115,32 @@ if __name__ == '__main__':
 
     hamiltonian = [
         HamiltonianData(-sigmaz()),
-        HamiltonianData(sigmax(), placeholder_callback)
+        HamiltonianData(-sigmax(), placeholder_callback)
     ]
 
-    # protocol = [0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0,
-    #             0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1]
-    # t = 3
-    # N = 60
-    # plot_h(t, protocol)
-    #
-    # test_protocol(
-    #     protocol,
-    #     hamiltonian,
-    #     t=t,
-    #     N=N,
-    #     initial_state=initial_state,
-    #     target_state=target_state
-    # )
+    protocol = [1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0,
+                0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1]
+    t = 3
+    N = 60
+    plot_h(t, protocol)
 
-    protocol_05 = [0, 0, 0, 0, 0, 1, 0, 0, 0, 0]
+    evaluate_protocol(
+        protocol,
+        hamiltonian,
+        t=t,
+        N=N,
+        initial_state=initial_state,
+        target_state=target_state
+    )
+
+    protocol_05 = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0]
 
     t = 0.5
     N = 10
     plot_h(t, protocol_05)
-
-    test_protocol(
+    # x = initial_state * 1 / initial_state.get_data()[1][0].toarray()[0][0].real
+    # y = target_state * 1 / target_state.get_data()[1][0].toarray()[0][0].real
+    evaluate_protocol(
         protocol_05,
         hamiltonian,
         t=t,
